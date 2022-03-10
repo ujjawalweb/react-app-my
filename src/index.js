854461ee6a8aa5d9b864f3ad4a49277419e39553
ujjawalweb <85484866+ujjawalweb@users.noreply.github.com>
2022-03-11 00:01:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import  BrowserRouter from "react-router-dom";
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
@@ -13,10 +13,10 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.render(
   <>
-
+<BrowserRouter>
   <App />
   
-    
+   </BrowserRouter>
 
   </>,
   document.getElementById('root')
