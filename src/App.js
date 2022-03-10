cc7298333e745a8d2ee628c2430b2255a3e90c9e
ujjawalweb <85484866+ujjawalweb@users.noreply.github.com>
2022-03-10 16:41:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import Home from './Home';
 
 import Contact  from './Contact';
 
-import {  Route, Switch } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
     <main>
-    <Switch>
+    <Routes>
     <Route path="/" component={Home} exact />
         <Route path="/aboutus" component={Aboutus} />
         <Route path="/services" component={Services} />
         <Route path="/contact" component={Contact} />
-    </Switch>
+    </Routes>
 </main>
 
 
