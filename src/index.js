baeeed1075b38a558ac2ccdf778101663a377fe7
ujjawalweb <85484866+ujjawalweb@users.noreply.github.com>
2022-03-11 09:51:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 
 
 ReactDOM.render(
-  <>
+  
 <BrowserRouter>
   <App />
   
    </BrowserRouter>
 
-  </>,
+ ,
   document.getElementById('root')
 );
 
